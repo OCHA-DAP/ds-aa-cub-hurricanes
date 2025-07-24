5292e86de3e0b22a28a85b9ab33fc976530f7bbe
Zack <zackarno@gmail.com>
2025-07-24 17:25:00 -0600

    remove updating of observational data from 01_update_fcast_data.py

--- pipelines/01_update_fcast_data.py
+++ pipelines/01_update_fcast_data.py
@@ -4,18 +4,16 @@ from src.utils.logging import get_logger
 logger = get_logger(__name__)
 
 
-def update_monitoring_data():
+def update_forecast_data():
     """Main function to run Cuba hurricane monitoring updates."""
     # Create monitor with IMERG rainfall processing
     monitor = monitoring_utils.create_cuba_hurricane_monitor(
         rainfall_source="raster"
     )
 
-    # Update both observational and forecast monitoring
-
+    # Update observational data
     monitor.update_monitoring("fcast", clobber=False)
-    monitor.update_monitoring("obsv", clobber=False)
 
 
 if __name__ == "__main__":
-    update_monitoring_data()
+    update_forecast_data()
